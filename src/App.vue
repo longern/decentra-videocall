b130e146d4c38ee83a82b782cd0b91ff4ef8fb30
longern <i@longern.com>
2021-03-21 22:30:28 +0800

    Share link using nativeshare

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,50 @@
 <template>
-  <button v-if="peerId && !friendPeerId" @click="copyPeerLink" class="btn-icon call-btn">
+  <button v-if="peerId && !friendPeerId" @click="sharePeerLink" class="btn-icon call-btn">
     <svg style="width:48px;height:48px" viewBox="0 0 24 24">
       <path fill="#00FF53" d="M6.62,10.79C8.06,13.62 10.38,15.94 13.21,17.38L15.41,15.18C15.69,14.9 16.08,14.82 16.43,14.93C17.55,15.3 18.75,15.5 20,15.5A1,1 0 0,1 21,16.5V20A1,1 0 0,1 20,21A17,17 0 0,1 3,4A1,1 0 0,1 4,3H7.5A1,1 0 0,1 8.5,4C8.5,5.25 8.7,6.45 9.07,7.57C9.18,7.92 9.1,8.31 8.82,8.59L6.62,10.79Z" />
     </svg>
   </button>
   <video ref="mainView" class="main-view"></video>
   <video ref="selfView" muted class="self-view"></video>
+  <div v-if="wechatSharing && !friendPeerId" class="share">点击右上角分享会话链接</div>
 </template>
 
 <script>
+import NativeShare from "nativeshare";
 import Peer from "peerjs";
+
+let peer = null;
+const nativeShare = new NativeShare();
 
 export default {
   name: "App",
   data() {
     const peerIdMatch = location.search.match(/p=([\da-f-]*)/);
     return {
-      peer: null,
       peerId: null,
-      friendPeerId: peerIdMatch ? peerIdMatch[1] : null
+      friendPeerId: peerIdMatch ? peerIdMatch[1] : null,
+      wechatSharing: false
     };
   },
 
   methods: {
-    copyPeerLink() {
+    sharePeerLink() {
       const url = new URL(window.location);
       url.searchParams.set("p", this.peerId);
-      window.history.pushState({}, "", url);
-      navigator.clipboard.writeText(window.location.href);
+      nativeShare.setShareData({
+        link: url,
+        title: "视频通话",
+        desc: "您的好友正邀请您进行视频通话"
+      });
+      try {
+        if (navigator.userAgent.indexOf("MicroMessenger") > -1) {
+          this.ua = navigator.userAgent.indexOf("MicroMessenger");
+          this.wechatSharing = true;
+          window.history.pushState({}, "", url);
+        } else nativeShare.call();
+      } catch (err) {
+        navigator.clipboard.writeText(url);
+      }
     }
   },
 
@@ -41,15 +58,15 @@ export default {
       });
     };
 
-    this.peer = new Peer();
-    this.peer.on("open", id => (this.peerId = id));
-    this.peer.on("call", call => {
+    peer = new Peer();
+    peer.on("open", id => (this.peerId = id));
+    peer.on("call", call => {
       this.friendPeerId = call.peer;
       call.answer(this.$refs.selfView.srcObject);
       bindMainView(call);
     });
-    this.peer.on("disconnected", () => this.peer.reconnect());
-    this.peer.on("error", err => {
+    peer.on("disconnected", () => peer.reconnect());
+    peer.on("error", err => {
       console.log(err);
       this.friendPeerId = null;
     });
@@ -60,8 +77,8 @@ export default {
         this.$refs.selfView.srcObject = mediaStream;
 
         if (this.friendPeerId) {
-          this.peer.on("open", () => {
-            bindMainView(this.peer.call(this.friendPeerId, mediaStream));
+          peer.on("open", () => {
+            bindMainView(peer.call(this.friendPeerId, mediaStream));
           });
         }
       });
@@ -76,10 +93,20 @@ body {
 }
 
 .btn-icon {
-  background: #333;
+  background: transparent;
   outline: none;
   border-radius: 50%;
+  border: none;
+  transition: background 0.5s;
 }
+
+  .btn-icon:hover {
+    background: #222;
+  }
+
+  .btn-icon:active {
+    background: #333;
+  }
 
 .call-btn {
   position: fixed;
@@ -104,5 +131,15 @@ body {
   max-width: 50vw;
   max-height: 30vh;
   transform: rotateY(180deg);
+}
+
+.share {
+  position: fixed;
+  top: 0;
+  right: 0;
+  z-index: 2;
+  padding: 8px;
+  background: rgba(200, 200, 200, 0.5);
+  border-radius: 4px;
 }
 </style>
